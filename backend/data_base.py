ba7254cb98f211550b800ee6117da57b4ff51ccf
AlexKulam <alexkulam45@gmail.com>
2025-07-25 18:31:39 +0300

    first commit

---
+++ backend/data_base.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+SQL_URL = "sqlite:///./DB/users.db"
+engine = create_engine(SQL_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+class Base(DeclarativeBase):
+    pass
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False) 
