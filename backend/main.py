c340cc1ecb0cf1da35a6d078db7a6f18b01b058f
AlexKulam <alexkulam45@gmail.com>
2025-07-25 18:58:36 +0300

    добавлен маин

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from data_base import Users, SessionLocal, Base, engine
+from fastapi.staticfiles import StaticFiles
+from backend.sign_up import router as sign_up_router
+from backend.log_in import router as log_in_router
+from backend.data_base import Base, engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,3 +15,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(sign_up_router)
+app.include_router(log_in_router)
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="static")
